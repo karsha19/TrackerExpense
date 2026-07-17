50ff4d9aa36412528c38245460f23c7cfb663df4
Raksha <rakshaadhikari697@gmail.com>
2026-07-17 14:05:46 +0530

    added

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const api = axios.create({ baseURL: 'http://localhost:8000' })
+const api =  import.meta.env.VITE_API_URL
 
 api.interceptors.request.use(cfg => {
   const token = localStorage.getItem('token')
